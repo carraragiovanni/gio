c9d3bae59d19500bd02aea5130740941fa72b50e
carraragiovanni <carrara.g@gmail.com>
2019-04-18 20:08:51 -0400

    make it better

--- public/main.js
+++ public/main.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     document.addEventListener('click', function (click) {
+        clearAllLinks();
         if (click.target.nodeName == "A") {
-            document.getElementById('right').style.display = "inline";
+            document.getElementById('right').style.display = "inline"
             populateRight(click.target.innerHTML);
         } else {
             document.getElementById('right').style.display = "none";
@@ -9,31 +10,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
     })
 });
 
+function clearAllLinks() {
+        var links = document.getElementsByClassName("links");
+
+        for (var i = 0; i < links.length; i++) {
+            links[i].style.display = "none";
+        }
+};
+
 function populateRight(type) {
     switch (type) {
         case "code":
-            document.getElementById("code-links").style.display = "inline";
-            document.getElementById("12M").style.display = "none";
-            document.getElementById("cad-links").style.display = "none";
-            document.getElementById("bicicletta").style.display = "none";
-            break;
-            case "CAD":
-            document.getElementById("cad-links").style.display = "inline";
-            document.getElementById("12M").style.display = "none";
-            document.getElementById("code-links").style.display = "none";
-            document.getElementById("bicicletta").style.display = "none";
-            break;
-            case "12M":
-            document.getElementById("12M").style.display = "inline";
-            document.getElementById("cad-links").style.display = "none";
-            document.getElementById("code-links").style.display = "none";
-            document.getElementById("bicicletta").style.display = "none";
-            break;
-            case "bicicletta":
-            document.getElementById("bicicletta").style.display = "inline";
-            document.getElementById("12M").style.display = "none";
-            document.getElementById("cad-links").style.display = "none";
-            document.getElementById("code-links").style.display = "none";
-            break;
+        document.getElementById("code-links").style.display = "inline";
+        break;
+        case "CAD":
+        document.getElementById("cad-links").style.display = "inline";
+        break;
+        case "12M":
+        document.getElementById("12M").style.display = "inline";
+        break;
+        case "bicicletta":
+        document.getElementById("bicicletta").style.display = "inline";
+        break;
+        case "about":
+        document.getElementById("about").style.display = "inline";
+        break;
     }
 }
